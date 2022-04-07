fe612a4a11309e88bb3b1d0ba3eb6d923dd50db3
Hafsa2001 <78575745+Hafsa2001@users.noreply.github.com>
2022-04-07 01:49:14 +0000

    Update TCPServer.py

--- TCPServer.py
+++ TCPServer.py
@@ -1,6 +1,5 @@
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print 'Server is Listening for incoming Client Requests!!!'
